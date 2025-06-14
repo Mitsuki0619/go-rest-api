451b5c38cd7047b41d4ec0d77b9938d4741389fa
Mitsuki0619 <m.iwamura1975@gmail.com>
2025-06-14 18:44:11 +0900

    環境変数が読み込まれない不具合解消

--- apps/server/db/db.go
+++ apps/server/db/db.go
@@ -11,13 +11,15 @@ import (
 )
 
 func NewDB() *gorm.DB {
+	err := godotenv.Load()
 	if os.Getenv("GO_ENV") == "dev" {
-		err := godotenv.Load()
 		if err != nil {
 			log.Fatalln(err)
 		}
 	}
-	url := fmt.Sprintf("postgres://%s:%s@%s:%s/%s", os.Getenv("POSTGRES_USER"), os.Getenv("POSTGRES_PASSWORD"), os.Getenv("POSTGRES_HOST"), os.Getenv("POSTGRES_PORT"), os.Getenv("POSTGRES_DB"))
+	url := fmt.Sprintf("postgres://%s:%s@%s:%s/%s", os.Getenv("POSTGRES_USER"),
+		os.Getenv("POSTGRES_PW"), os.Getenv("POSTGRES_HOST"),
+		os.Getenv("POSTGRES_PORT"), os.Getenv("POSTGRES_DB"))
 	db, err := gorm.Open(postgres.Open(url), &gorm.Config{})
 	if err != nil {
 		log.Fatalln(err)
